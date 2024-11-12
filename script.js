6b237e3a927b1988e59b3382ca4d85645454fe1f
Adriana <adrianacc20018@gmail.com>
2024-11-11 23:15:55 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Función que se ejecuta cuando se hace clic en el botón
-function showMessage() {
-    // Muestra una alerta con un mensaje
-    alert("¡Gracias por contactarme! Me pondré en contacto contigo pronto.");
-}
